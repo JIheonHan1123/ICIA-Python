aa0cf59af4919c21d9dc9dbb37474dcb7473bbe8
icia-jh <icia@a.com>
2024-01-10 10:00:15 +0900

    240110 1000

--- Day8-24.01.10/3.예외처리.py
+++ Day8-24.01.10/3.예외처리.py
@@ -5,6 +5,7 @@
 # except: 예외가 발생하면 실행할 코드
 
 # int 받아오는 코드에 다른 타입이 들어왔을 때 예외처리
+'''
 try:
     score = int(input('점수입력>> '))
     # 아래 코드는 try문 안으로 들어와야 함
@@ -16,3 +17,19 @@ try:
         print('불합격')
 except:
     print('점수를 숫자로 입력하세요')
+'''
+
+# ex1) 정수변수 2개를 만들어, 나눗셈 결과를 출력하시오
+# 예외처리가 필요하면 예외처리 하시오 -> 예외: 0으로 나눌 경우
+a = 10
+b = 0
+
+
+def nanutsam(a: int, b: int):
+    return a/b
+
+
+try:
+    print(nanutsam(a, b))
+except:
+    print('0으로 나눌 수 없습니다.')
