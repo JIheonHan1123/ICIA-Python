37d0c469e9c66f09fc60a8a73d3b388abc582471
Jiheon <hanjihh1123@gmail.com>
2024-01-18 16:34:05 +0900

    1634

--- B.Jan.18(2)/py_back/sever002.py
+++ B.Jan.18(2)/py_back/sever002.py
@@ -5,11 +5,12 @@ app.run()
 '''
 
 from flask import Flask, request, render_template
+# cf) render_template (템플릿을 그린다)
 
 app = Flask(__name__)
 
 
-# 사용자가 입력한 값을 html로 출력
+# ex1) 사용자가 입력한 값을 html로 출력
 @app.route('/sample1')
 def sample1():
     val = request.args['val']
@@ -21,6 +22,26 @@ def sample1():
     # html폴더 만들어주고 실행하면 되는데 만드는 방법이 다 정해져 있다!
     # 현재 서버파일과 같은 위치에 templates폴더 만들고 이 폴더 밑에 html파일 만든다
     # ex) html파일 만들고 F5로 실행하고 뒤에 주소에 /sample1?val=10
+
+
+# ex2) 사용자가 입력한 값들의 합을 html로 출력
+@app.route('/add')
+def sample2():
+    val1 = int(request.args['val1'])
+    val2 = int(request.args['val2'])
+    result = val1 + val2
+
+    # cf) 여기서 "sample2_.html"이름에 오타나면 500에러가 난다.
+    return render_template("sample2_.html", result=result)
+
+
+# 주소창에 직접 값을 입력하지 않게 만들어야함 ex3) 덧셈을 입력할 화면을 출력
+@app.route('/add_view')
+def sample2_view():
+    return render_template("sample2_view.html")
+    # 여기까지는 입력할 화면 띄워주는 코드고
+    # 값 입력하고 submit할 주소는 위에 만들었던 ~/add
+    # 차피 같은 서버(파일)에 있는거라 그냥 /add만 줘도 됨
 
 
 app.run()
